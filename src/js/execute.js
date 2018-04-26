6ebb33e5fc889d66e9be41796c406ca075df28ff
zjf <1010543618@qq.com>
2018-04-26 23:17:48 +0800

    Coding functions in ribbon.

--- src/js/execute.js
+++ src/js/execute.js
@@ -17,6 +17,22 @@ export default function(Code, Parameter) {
     alert('正在开发')
     return
   }
+
+  //演示工具
+  if (Code == 'createPresentation()') {
+    var itemId = SGWorld.ProjectTree.FindItem("演示工具");
+    if (itemId == "") {
+        itemId = SGWorld.ProjectTree.CreateGroup("演示工具");
+    }
+    var name = "演示工具#" + (Math.ceil(Math.random() * 100000));
+    SGWorld.Creator.CreatePresentation(itemId, name);
+    var Id = SGWorld.ProjectTree.FindItem("演示工具\\" + name);
+    if (Id != "") {
+        SGWorld.ProjectTree.SelectItem(Id);
+    }
+    return
+  }
+
   if (SGWorld.Command.CanExecute(Code, Parameter) == false) {
     alert("此工具当前不可用");
   } else {
